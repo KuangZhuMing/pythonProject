e8361ca43ea2ffa559a51fda7a7b70b9ea5409f2
kuangzhuming@163.com <KZMsr1102>
2023-03-13 11:32:04 +0800

    change file

--- my_electric_car.py
+++
@@ -1,8 +0,0 @@
-from car import ElectricCar
-
-my_tesla = ElectricCar('tesla','modle s',2022)
-
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.battery_size=50 #自己定义一个电池容器
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
--- number_writer.py
+++
@@ -1,8 +0,0 @@
-import json
-
-numbers = [1,3,4,5,6,7,8]
-
-filename = 'number.json'
-with open(filename,'w') as f_obj:
-    json.dump(numbers,f_obj)
-    
