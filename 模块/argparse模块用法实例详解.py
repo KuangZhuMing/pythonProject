3de59025d24377fa804e8b6b736cb175c2f53c33
kuangzhuming@163.com <KZMsr1102>
2023-03-13 11:49:47 +0800

    argparse

---
+++ 模块/argparse模块用法实例详解.py
@@ -0,0 +1,13 @@
+#argsparse是python的命令行解析的标准模块，内置于python，不需要安装。这个库可以让我们直接在命令行中就可以向程序中传入参数并让程序运行。
+
+#传入一个参数
+import argparse
+
+parser = argparse.ArgumentParser(description='命令行中传入一个数字')
+#type是要传入的参数的数据类型  help是该参数的提示信息
+parser.add_argument('integers', type=str, help='传入的数字')
+
+args = parser.parse_args()
+
+#获得传入的参数
+print(args)
